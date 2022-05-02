ad890e0ce0e02e552379fa9e0a168a726f3429cb
Andrew Xia <axia@mit.edu>
2022-05-02 16:50:38 -0700

    get more images, add click to photos page

--- pages/p3.js
+++ pages/p3.js
@@ -60,15 +60,8 @@ export default function MyModal() {
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
-                    Payment successful
+                    Image
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We've sent
-                      you an email with all of the details of your order.
-                    </p>
-                  </div>
-
                   <div className="mt-4">
                     <Image
                       alt=""
@@ -93,7 +86,7 @@ export default function MyModal() {
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Got it, thanks!
+                      Close
                     </button>
                   </div>
                 </Dialog.Panel>
