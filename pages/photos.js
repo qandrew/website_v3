94e2c0c73c85cd3937716a0f205d56fdca018845
Andrew Xia <axia@mit.edu>
2022-05-09 11:58:43 -0700

    at least image is full width now on mobile, and not too wide on wide ratios

--- pages/photos.js
+++ pages/photos.js
@@ -79,20 +79,14 @@ function BlurImage({ image }) {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full max-h-screen transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900"
-                    >
-                      {image.name}
-                    </Dialog.Title>
+                  <Dialog.Panel className="w-full h-5/6 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                     <div className="mt-4 max-h-5/6">
                       <Image
                         alt=""
                         src={image.imageSrc}
-                        width="80%"
-                        height="40%"
-                        layout="responsive"
+                        // width="80%"
+                        // height="40%"
+                        layout="fill"
                         objectFit="contain"
                         className={cn(
                           'group-hover:opacity-75 duration-700 ease-in-out',
@@ -107,7 +101,7 @@ function BlurImage({ image }) {
                     <div className="mt-4">
                       <button
                         type="button"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        className="relative inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                         onClick={closeModal}
                       >
                         Close
