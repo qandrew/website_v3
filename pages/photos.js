568a1da6e2c9e85d8caef218a2d4a86bc07fa749
Andrew Xia <axia@mit.edu>
2022-05-12 10:47:33 -0700

    change padding

--- pages/photos.js
+++ pages/photos.js
@@ -119,7 +119,7 @@ function BlurImage({ image }) {
 
 function Photos({ images }) {
   return (
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
         <h1 className="pb-4 mt-4 text-lg font-medium">Selected Photos</h1>
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {images.map((image) => (
