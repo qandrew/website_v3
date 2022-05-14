1e19240c3e0ddc18bef05bc7da4561cf97aff45a
Andrew Xia <axia@mit.edu>
2022-05-14 09:49:37 -0700

    clean up

--- components/layout.js
+++ components/layout.js
@@ -42,10 +42,10 @@ export default function Layout({ children, home, post }) {
       {/* TODO: header / padding shifts when an image is in markdown file */}
       <nav className="px-2 sm:px-4 py-2.5 mx-auto bg-white rounded">
         <div className="flex flex-wrap justify-between items-center">
-          <a href="/" className="flex items-center">
-              <img src={imageLoc} className="rounded-full mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <Link href="/">
+              <a className="flex items-center"><img src={imageLoc} className="rounded-full mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Andrew Xia</span>
-          </a>
+          </a></Link>
           <button
             data-collapse-toggle="mobile-menu"
             type="button"
@@ -55,8 +55,8 @@ export default function Layout({ children, home, post }) {
             onClick={() => setNavbarOpen(!navbarOpen)}
           >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </button>
           <div className={"w-full md:block md:w-auto" + (navbarOpen ? " flex" : " hidden")} id="mobile-menu">
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
@@ -77,7 +77,7 @@ export default function Layout({ children, home, post }) {
       {!home && (
         <div className="mt-4">
           <Link href="/">
-            <a>← Back to home</a>
+            <a>← Home</a>
           </Link>
         </div>
       )}
