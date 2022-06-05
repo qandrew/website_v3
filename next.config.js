a7d6b5dd1750b652f3312515e20fa01172ba3e07
Andrew Xia <axia@mit.edu>
2022-06-05 17:10:30 -0500

    remove dependency on fb images (#16)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   images: {
     domains: [
-      "upload.wikimedia.org",
-      "scontent-sjc3-1.xx.fbcdn.net",
-      "scontent-sea1-1.xx.fbcdn.net",
-      "pbs.twimg.com",
-      "dgtzuqphqg23d.cloudfront.net",
       "lh3.googleusercontent.com",
     ],
   },
