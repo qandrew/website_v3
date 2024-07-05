2b6da612eed704473ab14fba28c384649f9b5bc7
Andrew Xia <axia@mit.edu>
2024-07-05 14:17:51 -0700

    add us states overlay

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,5 @@
-// MarkerPopupMap.js
-import React, { useEffect } from "react";
-import Point from "ol/geom/Point.js";
+import GeoJSON from "ol/format/GeoJSON";
+import React, { useEffect, useState } from "react";
 import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
@@ -9,23 +8,15 @@ import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
 import Overlay from "ol/Overlay";
 import { toLonLat } from "ol/proj.js";
 import { toStringHDMS } from "ol/coordinate.js";
-import Feature from "ol/Feature.js";
 
 export default function map() {
   useEffect(() => {
-    const iconFeature = new Feature({
-      geometry: new Point([0, 0]),
-      name: "Null Island",
-      population: 4000,
-      rainfall: 500,
-    });
 
-    const vectorSource = new VectorSource({
-      features: [iconFeature],
-    });
-
-    const vectorLayer = new VectorLayer({
-      source: vectorSource,
+    const statesLayer = new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: "./map/us-states.json",
+      }),
     });
 
     const rasterLayer = new TileLayer({
@@ -36,8 +27,6 @@ export default function map() {
     });
 
     const container = document.getElementById("popup");
-    const content = document.getElementById("popup-content");
-    const closer = document.getElementById("popup-closer");
     const overlay = new Overlay({
       element: container,
       autoPan: {
@@ -48,7 +37,7 @@ export default function map() {
     });
 
     const map = new Map({
-      layers: [rasterLayer, vectorLayer],
+      layers: [rasterLayer, statesLayer],
       target: "markerpopupmap",
       view: new View({
         center: [0, 0],
@@ -60,22 +49,34 @@ export default function map() {
     /**
      * Add a click handler to the map to render the popup.
      */
-    map.on("singleclick", function (evt) {
+    map.on("singleclick", async function (evt) {
       const coordinate = evt.coordinate;
       const hdms = toStringHDMS(toLonLat(coordinate));
+      const stateFeatures = await statesLayer.getFeatures(evt.pixel)
+      let stateName: string = ''
+      if (stateFeatures.length > 0) {
+        stateName = stateFeatures[0].get('name')
+      }
+      setPopupContent(`${hdms} in ${stateName}`)
 
-      content.innerHTML = "<p>You clicked here:</p><code>" + hdms + "</code>";
+
+      // content.innerHTML = "<p>You clicked here:</p><code>" + hdms + "</code>";
       overlay.setPosition(coordinate);
     });
     return () => map.setTarget(null);
   }, []);
+
+  const [popupContent, setPopupContent] = useState<string | null>(null);
 
   return (
     <div>
       <div id="markerpopupmap" style={{ width: "100%", height: "100vh" }} />
       <div id="popup" className="ol-popup" style={{ backgroundColor: "#fff" }}>
         <a href="#" id="popup-closer" className="ol-popup-closer"></a>
-        <div id="popup-content"></div>
+        {popupContent && (
+          <div id="popup-content">You clicked here: {popupContent}
+          {' '}<button onClick={() => setPopupContent(null)}>Close</button></div>
+        )}
       </div>
     </div>
   );
